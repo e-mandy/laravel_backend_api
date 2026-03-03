aeb935adf91bfa79b0e2d04258e761210debda4b
e-mandy <andymfrd02@gmail.com>
2026-03-03 12:57:42 +0000

    fix(register-service): Correction of the hash of the password before creating user

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -2,6 +2,18 @@
 
 namespace App\Services;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 class AuthService{
-    
+    public function register(Array $data): User 
+    {
+        $data['password'] = Hash::make($data['password']);
+        $user = User::create([
+            ...$data
+            ]);
+        
+        return $user;
+    }
+
 }
