a992d4bf72fe7efc8050a99f58d7f8f72ccdf35b
e-mandy <andymfrd02@gmail.com>
2026-03-05 00:14:26 +0000

    refactor(routes): Integrated the chaining method

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::middleware(['auth:api'])->group(function(){
-    Route::post('/logout', [AuthController::class, 'logout']);
+Route::middleware(['auth:apit -mi'])
+    ->group(function(){
+        Route::post('/logout', [AuthController::class, 'logout']);
 });
